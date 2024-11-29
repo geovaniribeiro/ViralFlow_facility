386d7ddc22f1de348d7f5ac12b6eb52a2938a1d5
Geovani Ribeiro <geovanibiotec@gmail.com>
2024-11-29 11:46:38 -0300

    Setup automatized

--- Installer/viralflow_setup.sh
+++ Installer/viralflow_setup.sh
@@ -3,18 +3,6 @@
 # Update and install necessary packages
 #sudo apt update -y && \
 #sudo apt upgrade -y && \
-sudo apt install curl git python3-pip uidmap -y
-
-#Install complement libraty and tools
-sudo apt install dconf-editor
-pip install pyqt5
-sudo apt install python3 python3-tk
-
-#deixar os scripts executaveis
-#chmod +x viralflow_GUI_sc2
-#chmod +x scripts/viralflow_GUI_sc2.py
-#chmod +x viralflow_GUI_sc2
-
 code_path=$(pwd)
 
 # Download and set up Micromamba
@@ -22,29 +10,46 @@ cd $HOME
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/1.5.7 | tar -xvj bin/micromamba
 ./bin/micromamba shell init -s bash -p ~/micromamba
 source ~/.bashrc
+
+# Carregar manualmente as alterações feitas no ~/.bashrc no ambiente atual
+export MAMBA_EXE="$HOME/bin/micromamba"
+export MAMBA_ROOT_PREFIX="$HOME/micromamba"
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback para ativar manualmente, se necessário
+fi
+unset __mamba_setup
+
 micromamba activate
 
 #Remove previous viraflow instalation
-#rm -r ViralFlow/
+sudo rm -r ViralFlow/
 #micromamba env remove -n viralflow
 
 # Clone ViralFlow repository and set up the environment
-#git clone https://github.com/WallauBioinfo/ViralFlow
+git clone -b develop_fixMicromambaOnPangolin https://github.com/WallauBioinfo/ViralFlow
 cd ViralFlow/
 micromamba env create -f envs/env.yml
 micromamba activate viralflow
+
+#Instalar os 
 pip install -e .
 
 # Create symbolic link for unsquashfs
 sudo ln -s /usr/bin/unsquashfs /usr/local/bin/unsquashfs
 
-# Build containers
+# Download image and Build containers
 viralflow -build_containers
 
 ###################
 #Instalar bibliotecas adicionais para gerar os arquivos e relatorios
-pip install -r $code_path/envs/env.yml
+pip install -r $code_path/../envs/env.yml
 
-echo '###################################################################';
-echo '################## ViralFlow Atualizado! ##########################';
-echo '###################################################################';
+echo ""
+echo ""
+echo '##########################################################################';
+echo '########################## ViralFlow Instalado! ##########################';
+echo '##################### Esse terminal pode ser fechado #####################';
+echo '##########################################################################';
