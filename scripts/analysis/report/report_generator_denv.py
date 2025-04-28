627880f56660decece2206a4e1f574fb6a815ec2
Geovani Ribeiro <geovanibiotec@gmail.com>
2025-04-28 09:42:21 -0300

    Correção nome planilha DENV

--- scripts/analysis/report/report_generator_denv.py
+++ scripts/analysis/report/report_generator_denv.py
@@ -647,7 +647,7 @@ def generate_report_denv(metadata_path, config_path, output_folder):
     Quality_monitor(coverage, reads, output_folder)
 
     # Limpar arquivos temporários e monitorar qualidade
-    #remover_csv(output_folder)
+    remover_csv(output_folder)
 
 # Mantém a funcionalidade standalone
 if __name__ == "__main__":
