8ce5ddc1505a00166ecab2ace08a01a79748b0cf
Geovani Ribeiro <geovanibiotec@gmail.com>
2025-04-28 09:42:21 -0300

    Correção nome planilha DENV

--- scripts/analysis/report/report_generator_chikv.py
+++ scripts/analysis/report/report_generator_chikv.py
@@ -103,7 +103,7 @@ def generate_report_chikv(metadata_path, config_path, output_folder):
                         arbo_virus_name_value = "Chikungunya virus", seq_id_fixed= "ChikV")
 
         epi_arbo_file = os.path.join(output_folder, 'RNSG_REPORT', 'EpiArbo.csv')
-        #Removendo as colunas "arbo_subtype" e "arbo_last_vaccination_date" que estão no EpiArbo (DENV) mas não estão no EpiArbo (CHIKV)
+        #Removendo as colunas "arbo_subtype" e "arbo_last_vaccination_date" que estão no EpiArbo DENV mas não estão no EpiArbo CHIKV
         colunas_remover = ['arbo_subtype', 'arbo_last_vaccination_date']
         df_epi_arbo = pd.read_csv(epi_arbo_file)
         df_epi_arbo = df_epi_arbo.drop(columns=colunas_remover, errors='ignore')
