4b14112846b5b07bf6ae04d01126376b9f65e150
Geovani Ribeiro <geovanibiotec@gmail.com>
2025-09-25 17:37:25 -0300

    ajuste pyside6

--- scripts/analysis/assembler/assembler_thread.py
+++ scripts/analysis/assembler/assembler_thread.py
@@ -4,15 +4,15 @@ import sys
 import os
 import subprocess
 
-from PyQt5.QtCore import QThread, pyqtSignal
+from PySide6.QtCore import QThread, Signal
 
 # Adiciona o diretório raiz do projeto ao PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 # Classe para executar o processo em um thread separado
 class AssemblerThread(QThread):
-    process_started = pyqtSignal(str)
-    process_finished = pyqtSignal(str)
+    process_started = Signal(str)
+    process_finished = Signal(str)
 
     def __init__(self, commands=None):
         super().__init__()
